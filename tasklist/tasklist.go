ea35d5de5d498e498cf677ccad46aca05bc7cfbc
jcarr132 <joshcarr132@gmail.com>
2020-10-15 19:21:31 -0400

    added some documentation comments

--- tasklist/tasklist.go
+++ tasklist/tasklist.go
@@ -1,3 +1,8 @@
+/*
+Tasklist handles interaction with the JSON database (`simdb`). This package
+defines the TaskList struct type, which wraps the database connection, and
+the Task struct, which holds data for a single task.
+*/
 package tasklist
 
 import (
@@ -5,10 +10,18 @@ import (
 	"github.com/sonyarouje/simdb/db"
 )
 
+/*
+TaskList simply wraps the database driver and provides methods for acting
+on the list as a whole.
+*/
 type TaskList struct {
 	Db db.Driver
 }
 
+/*
+NewTaskList returns a new TaskList struct containing a connection to the
+`simdb` database.
+*/
 func NewTasklist() TaskList {
 	driver, err := db.New("data")
 	if err != nil {
@@ -20,6 +33,10 @@ func NewTasklist() TaskList {
 	}
 }
 
+/*
+Tasks() queries the database returns a slice containing the tasks stored
+within.
+*/
 func (tl TaskList) Tasks() []Task {
 	var tasks []Task
 	err := tl.Db.Open(Task{}).Get().AsEntity(&tasks)
@@ -30,14 +47,19 @@ func (tl TaskList) Tasks() []Task {
 	return tasks
 }
 
+/*
+The Task struct holds data about a task. Each Task is assigned a random UUID
+which is used as it's primary identifier.
+*/
 type Task struct {
 	TaskID uuid.UUID
 	Name   string `json:"name"`
 	Notes  string `json:"notes"`
 }
 
-// func TaskFromJSON()
-
+/*
+Task implements ID to conform to the database library (`simdb`) requirements.
+*/
 func (t Task) ID() (jsonField string, value interface{}) {
 	value = t.TaskID
 	jsonField = "id"
